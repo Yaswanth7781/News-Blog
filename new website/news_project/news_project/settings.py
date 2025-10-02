442f3e933ef0747045d7684467cc6b927bc57607
Yaswanth7781 <149923270+Yaswanth7781@users.noreply.github.com>
2025-10-02 21:15:37 +0530

    Update settings.py

--- new website/news_project/news_project/settings.py
+++ new website/news_project/news_project/settings.py
@@ -3,6 +3,10 @@ Django settings for news_project project.
 """
 
 from pathlib import Path
+import dj_database_url
+from decouple import config
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -66,17 +70,19 @@ WSGI_APPLICATION = 'news_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'news_db',
+#         'USER': 'yashdb',
+#         'PASSWORD': 'Yash@90325',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'news_db',
-        'USER': 'yashdb',
-        'PASSWORD': 'Yash@90325',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(default=config('postgres://yashdb:Yash@90325@localhost:5432/news_db'))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -122,6 +128,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
 
 
 
